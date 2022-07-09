11fb5ba4fa77048c3c5949c8ea23898c979ecaec
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-07-10 00:49:13 +0900

    주문조회 API V1, V2

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,10 +1,10 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
